7985a3f5fd03e2c4e8262d7309c25b071d7bab8a
naiqi.zhang <fireicewater001@163.com>
2020-06-24 14:21:29 +0800

    增加分页插件

---
+++ src/main/java/com/sim/sapjco/config/MybatisConfig.java
@@ -0,0 +1,26 @@
+package com.sim.sapjco.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author zhangnaiqi
+ * @version 1
+ * @date 2020/6/24 14:19
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.sim.sapjco.dao")
+public class MybatisConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
+}
